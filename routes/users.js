4ba07d77d26b855d8781584b92841c1251e2d47d
siddhu003 <113658076+siddhu003@users.noreply.github.com>
2023-04-01 00:47:16 +0530

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,6 @@
 import express from 'express'
-import { login, signup } from '../controllers/auth.js'
-import { getAllUsers, updateProfile } from '../controllers/users.js'
+import { login, signup, forgotPassword } from '../controllers/auth.js'
+import { getAllUsers, updateProfile, updateFollow, updateUnFollow } from '../controllers/users.js'
 import auth from '../middleware/auth.js'
 
 const router = express.Router()
@@ -10,5 +10,8 @@ router.post("/login", login)
 
 router.get('/getAllUsers', getAllUsers)
 router.patch('/update/:id', auth, updateProfile)
+router.post('/forgotPassword', forgotPassword)
+router.patch('/updatefollow/:id', updateFollow)
+router.patch('/updateunfollow/:id',updateUnFollow)
 
 export default router
