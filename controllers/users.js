64849a55958cc0def0a585785e0081239f4279a7
siddhu003 <113658076+siddhu003@users.noreply.github.com>
2023-04-01 00:46:36 +0530

    Update users.js

--- controllers/users.js
+++ controllers/users.js
@@ -7,7 +7,7 @@ export const getAllUsers = async (req, res) => {
         const allUserDetails = []
 
         allUsers.forEach(users => {
-            allUserDetails.push({ _id: users._id, name: users.name, about: users.about, tags: users.tags, joinedOn: users.joinedOn })
+            allUserDetails.push({ _id: users._id, name: users.name, about: users.about, tags: users.tags, joinedOn: users.joinedOn, follow: users.follow })
         })
 
         res.status(200).json(allUserDetails);
@@ -42,5 +42,86 @@ export const updateCurrentPlan = async (req, res) => {
         res.status(200).json(updatedProfile);
     } catch (error) {
         res.status(405).json({ message: error.message });
+    }
+}
+
+export const updateFollow = async (req, res) => {
+    const { id: _id } = req.params;
+    // const { followId, followName } = req.body;
+    const { followId} = req.body;
+    
+    try {
+        //const updatedProfile = await User.findByIdAndUpdate(_id, { $push: { 'follow': followId } });
+        //res.status(200).json(updatedProfile)
+        const user = await User.findById(_id);
+
+        const followIndex = user.follow.findIndex((id) => id === String(followId))  
+
+        if (followIndex === -1)
+        {
+            user.follow.push(followId);
+            //await User.findByIdAndUpdate(_id, { $addToSet: { 'follow':[{followId,followName}]} })
+        }
+        // user.follow.addToSet([{ followId, followName }]);
+        // await User.findByIdAndUpdate(_id, { $addToSet: { 'follow':[{followId,followName}]} })
+        await User.findByIdAndUpdate(_id, user);
+        //res.status(200).json({ message: "Successful" })
+
+        const user1 = await User.findById(followId);
+        const followerIndex = user1.followers.findIndex((id) => id === String(_id))
+
+        if (followerIndex === -1)
+        {
+            user1.followers.push(_id);
+        }
+        await User.findByIdAndUpdate(followId, user1);
+
+        res.status(200).json(user)   
+
+        //res.status(200).json(followId)
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
+}
+
+export const updateUnFollow = async (req, res) => {
+    const { id: _id } = req.params;
+    const { followId } = req.body;
+    
+    try {
+
+        const user = await User.findById(_id);
+        // await Questions.updateOne(
+        //     { _id },
+        //     { $pull: { 'answer': { _id: answerId } } }
+        // )
+        
+        // await User.updateOne(
+        //     { id },
+        //     { $pull: { 'follow': { _id: followId } } }
+        // )
+        const followIndex = user.follow.findIndex((id) => id === String(followId))  
+
+        if (followIndex !== -1)
+        {
+            user.follow.pull(followId);
+            //await User.findByIdAndUpdate(_id, { $addToSet: { 'follow':[{followId,followName}]} })
+        }
+        await User.findByIdAndUpdate(_id, user);
+
+
+        const user1 = await User.findById(followId);
+        const followerIndex = user1.followers.findIndex((id) => id === String(_id))
+
+        if (followerIndex !== -1)
+        {
+            user1.followers.pull(_id);
+        }
+        await User.findByIdAndUpdate(followId, user1);
+
+        res.status(200).json(user);
+        
+    } catch (error) {
+        res.status(400).json({ message: error.message });
     }
 }
