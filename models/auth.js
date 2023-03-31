f38df3973a9aefefdf2663a67d18642e1ca6b215
siddhu003 <113658076+siddhu003@users.noreply.github.com>
2023-04-01 00:47:44 +0530

    Update auth.js

--- models/auth.js
+++ models/auth.js
@@ -4,10 +4,17 @@ const userSchema = mongoose.Schema({
     name: { type: String, required: true },
     email: { type: String, required: true },
     password: { type: String, required: true },
-    about: { type: String},
+    about: { type: String, default: " "},
     tags: { type: [String]},
     joinedOn: { type: Date, default: Date.now },
-    accountType: { type: String, default: 'Free' }
+    accountType: { type: String, default: 'Free' },
+    //follow: [{ followid: String }]
+    follow: { type: [String], default: [] },
+    followers: { type: [String], default: [] }
+    // follow: [{
+    //     followId: String,
+    //     followName: String
+    // }]
 })
 
 export default mongoose.model("User",userSchema)
